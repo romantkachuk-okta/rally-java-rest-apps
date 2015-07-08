b9489183bea9e3102cb7010642ca7e6239cd3a2b
romantkachuk-okta <roman.tkachuk@okta.com>
2015-07-08 19:27:23 +0300

    Update ClearCustomUserTextField.java
    A way to reproduce error: "Could not write changes in Unit Of Work"

--- ClearCustomUserTextField.java
+++ ClearCustomUserTextField.java
@@ -4,62 +4,55 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.rallydev.rest.RallyRestApi;
-import com.rallydev.rest.util.Fetch;
-
-import com.rallydev.rest.request.QueryRequest;
 import com.rallydev.rest.request.UpdateRequest;
 import com.rallydev.rest.response.UpdateResponse;
-import com.rallydev.rest.response.QueryResponse;
-import com.rallydev.rest.util.QueryFilter;
 import java.util.Date;
 
 
 public class ClearCustomUserTextField{
-    public static void main(String[] args) throws URISyntaxException, IOException {
 
+    private static final String SERVER_URL = "https://rally1.rallydev.com";
+    private static final String API_KEY = "_cUi7..."; //use your ApiKey
 
-        String host = "https://rally1.rallydev.com";
-        String apiKey = "_abc123"; //use your ApiKey
-        String applicationName = "ClearCustomUserTextField";
+    private static final String TEXT_FIELD = "c_Text"; // Name of the field to change
 
+    private static final String USER_REF = "https://rally1.rallydev.com/slm/webservice/v2.0/user/34496097048"; // Referece to a User
 
-        RallyRestApi restApi = new RallyRestApi(new URI(host),apiKey);
-        restApi.setApplicationName(applicationName);
-
-        try {
-            QueryRequest userRequest = new QueryRequest("User");
-            userRequest.setFetch(new Fetch("UserName", "DisplayName", "c_Comments"));
-            userRequest.setQueryFilter(new QueryFilter("UserName", "=", "nick@denver.com"));
-            QueryResponse userQueryResponse = restApi.query(userRequest);
-            JsonArray userQueryResults = userQueryResponse.getResults();
-            JsonElement userQueryElement = userQueryResults.get(0);
-            JsonObject userObject = userQueryElement.getAsJsonObject();
-            String userRef = userObject.get("_ref").getAsString();
-            System.out.println(userObject.get("UserName") + " Comments field value: " + userObject.get("c_Comments"));
-            if(userObject.get("c_Comments").isJsonNull()){
-                String newComment = new Date().getTime()+""; //converting long to string
-                System.out.println("Comments field is empty. Updating to: " + newComment);
-                update(restApi, userRef, newComment);
-            }
-            else{
-                System.out.println("not empty");
-                System.out.println("Comments field is not empty. Clearing the value...");
-                update(restApi, userRef, "");
-            }
-        } finally {
-            //Release all resources
-            restApi.close();
-        }
-
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        ClearCustomUserTextField.fillField();
+        ClearCustomUserTextField.clearField();
     }
+
+    private static RallyRestApi createRestApi() throws URISyntaxException {
+        RallyRestApi restApi = new RallyRestApi(new URI(SERVER_URL), API_KEY);
+        restApi.setApplicationName("ClearCustomUserTextField");
+
+        return restApi;
+    }
+
+    public static void clearField() throws URISyntaxException, IOException {
+        System.out.println("Clearing the value...");
+
+        try (RallyRestApi restApi = createRestApi()) {
+            update(restApi, USER_REF, "");
+        }
+    }
+
+    public static void fillField() throws URISyntaxException, IOException {
+
+        String newComment = new Date().getTime()+""; //converting long to string
+        System.out.println("Updating value to: " + newComment);
+
+        try (RallyRestApi restApi = createRestApi()) {
+            update(restApi, USER_REF, newComment);
+        }
+    }
+
     private static void update(RallyRestApi restApi, String userRef, String comment ) throws URISyntaxException,IOException {
         JsonObject userUpdate = new JsonObject();
-        userUpdate.addProperty("c_Comments", comment);
+        userUpdate.addProperty(TEXT_FIELD, comment);
         UpdateRequest updateRequest = new UpdateRequest(userRef,userUpdate);
         UpdateResponse updateResponse = restApi.update(updateRequest);
 
@@ -74,5 +67,4 @@ public class ClearCustomUserTextField{
             }
         }
     }
-
 }
